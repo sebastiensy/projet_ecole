667bf802d3c011e520c9d4f6f2c651215bb9f760
florianD <floriand_@hotmail.fr>
2014-11-20 15:07:26 +0100

    ajout dossiers

---
+++ data/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('LIB', '../../lib');
+define('INC', '../../inc');
+define('IMG', '../../img');
+define('HOME', '../../accueil');
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB'  , 'projet_ecole');
+
+?>
