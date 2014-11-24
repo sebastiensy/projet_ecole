a61299771a1d4036a82183853184e5c4c055babf
sebastiensy <seb6890@gmail.com>
2014-11-24 16:48:33 +0100

    mise en page de etat de la commande

--- web/suivi/index.php
+++ web/suivi/index.php
@@ -2,6 +2,30 @@
 
 require_once('../../inc/data.inc.php');
 
+?>
+
+<body id="back">
+
+	<div id="banner">
+	</div>
+	
+	<div class="menu">
+		<div id="menu">
+			<div id="menu1">
+				<a href="../"><img src="../../img/menu/acceuil.png"></a>
+				<a href="../fournitures/"><img src="../../img/menu/article.png"></a>
+				<a href="../inscription/"><img src="../../img/menu/inscription.png"></a>
+				<a href="../contact/"><img src="../../img/menu/contact.png"></a>
+				</div>
+			</div>
+	</div>
+	
+	<div id="page">
+
+	
+
+<?php
+
 // Pour test
 $id_parent = 3;
 
@@ -78,7 +102,13 @@ session_destroy();
 
 $db->DB_done();	
 
+echo "</div>";
 
 
+?>
+
+<?php
+
+require_once(INC.'/footer.inc.php');
 
 ?>
