98e1b18ab26b9e7e45a50fda1d1c318a057d29d7
sebastiensy <seb6890@gmail.com>
2014-12-04 17:29:39 +0100

    fancy box pour etat de la commande et suivi de commande

--- web/admin/suivi.php
+++ web/admin/suivi.php
@@ -14,12 +14,17 @@ INSERT INTO Commande (id_commande, date_cmd, etat, id_parent) VALUES
 ('5', '2014-11-01', 5, 5)
 */ -->
 
+<body id="back">
 
+<div id="banner">
+</div>
+
+<div id="page">
 <?php
 
 	require_once('../../inc/data.inc.php');
 
-	$requete = 'SELECT p.nom_parent, c.etat FROM Parent as p, Commande as c WHERE p.id_parent = c.id_parent';
+	$requete = 'SELECT p.nom_parent, c.etat, c.id_commande FROM Parent as p, Commande as c WHERE p.id_parent = c.id_parent';
 
 	$db = new DB_connection();
 	$db->DB_query($requete);
@@ -28,13 +33,15 @@ INSERT INTO Commande (id_commande, date_cmd, etat, id_parent) VALUES
 
 	echo "<table>
 			<tr>
-				<th>Parent</th>
+				<th>Parenté</th>
 				<th>En cours de validation</th>
 				<th>Valide</th>
 				<th>Commande fournisseur</th>
 				<th>En cours de livraison</th>
 				<th>Livre</th>
+				<th></th>
 			</tr>";
+
 	
 	while($suiv = $db->DB_object())
 	{
@@ -48,13 +55,21 @@ INSERT INTO Commande (id_commande, date_cmd, etat, id_parent) VALUES
 			else
 			{
 				echo '<td><input type="checkbox" name="suivi'.$suiv->nom_parent.'" value="'.$i.'" disabled></td>';
-			}		
+			}
 		}
-
+		echo '<td><a id="fancy" value="commande'.$suiv->nom_parent.'" href="commande.php\?com='.$suiv->id_commande.'&nom='.$suiv->nom_parent.'">Etat de la commande</a></td>';		
 		echo "</tr>";
 	}
 
 	echo "</table>";
 
 	$db->DB_done();
+?>
+
+</div>
+
+<?php
+
+require_once(INC.'/footer.inc.php');
+
 ?>
