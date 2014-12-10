d01d9f0bfc201aae19f71cc165afb578ebc1b584
amani12 <amani.younes2@gmail.com>
2014-12-10 14:56:21 +0100

    a

--- admin/gestion_liste/ajouter_article_liste1.php
+++ admin/gestion_liste/ajouter_article_liste1.php
@@ -1,13 +1,14 @@
 <?php
-	require_once('../conf.php');
+	require_once('../../inc/data.inc.php');
 	$id=$_POST['id'];
 	$ref=$_POST['ref'];
 	$qte=$_POST['qte'];
+	$db = new DB_connection();
 	
 	if($ref!="" and $qte!="")
 	{
 		$req="insert into compose values('".$qte."','".$ref."','".$id."')";
-		$myql_result=mysqli_query($req,$connexion);
+		$db->DB_query($req);
 	}
 	$url="modif_liste.php?id=".$id;
 	
