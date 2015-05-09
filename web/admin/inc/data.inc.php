77f538160cefc635b1e9d2e56d72b9739d36404b
florianD <floriand_@hotmail.fr>
2015-05-09 18:06:28 +0200

    partie admin, bouton "valider" fonctionnel

--- web/admin/inc/data.inc.php
+++ web/admin/inc/data.inc.php
@@ -1,5 +1,7 @@
 <?php
-	require_once('../../../data/config.php');
-	require_once('../../../inc/url.inc.php');
-	require_once('../../../lib/lib_db.class.php');
+
+require_once('../../../data/config.php');
+require_once('../../../inc/url.inc.php');
+require_once('../../../lib/lib_db.class.php');
+
 ?>
