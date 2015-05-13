6ff6e928b834f8b3377d73a3d78b804adc745d76
sebastiensy <seb6890@gmail.com>
2015-05-13 11:39:12 +0200

    suppression commentaires

--- lib/lib_form_ajout_article_admin.php
+++ lib/lib_form_ajout_article_admin.php
@@ -1,4 +1,3 @@
-	<div id="ajout_article">
 <?php 
 
 function formulaire_ajout_article($msg="", $ref="", $desc="", $prix="")
@@ -12,58 +11,36 @@ function formulaire_ajout_article($msg="", $ref="", $desc="", $prix="")
 	<form method="post" action="ajouter_article.php" name="f1" id="maj">
 		<table width="90%" align="center">
 			<tr>
-			<td >
-				Reference
-			</td>
-			
-			<td >
-			Description
-					
-					</td>
-					<td> Prix</td>  
+				<td>Reference</td>
+				<td>Description</td>
+				<td>Prix</td>  
 			</tr>
-					
-			<tr >
-				<td >
-						<span><input type="text" name="ref" value="<?php echo $ref;?>" required></span>
-				
-				</td>
-				<td >
-					
-								<span><input size="30" type="text" name="desc" value="<?php echo $desc;?>"required></span>
-					
-				</td>
-				<td >
-					
-								<span><input size="10" type="text" name="prix" value="<?php echo $prix;?>" required></span>
-					
-				</td>
-				
-				</tr>
-				<tr>
-				<td colspan="3">
-				Categorie <select name="categorie">
-						<?php
+			<tr>
+				<td><span><input type="text" name="ref" value="<?php echo $ref;?>" required></span></td>
+				<td><span><input size="30" type="text" name="desc" value="<?php echo $desc;?>"required></span></td>
+				<td><span><input size="10" type="text" name="prix" value="<?php echo $prix;?>" required></span></td>
+			</tr>
+			<tr>
+				<td colspan="3">Categorie
+					<select name="categorie">
+					<?php
 						$db = new DB_connection();
-						$req="select distinct categorie from Sous_categorie order by categorie asc";
+						$req="SELECT DISTINCT categorie FROM Sous_categorie ORDER BY categorie ASC";
 						$db->DB_query($req);
 						while($ligne=$db->DB_object())
 						{
-						?>
-						<option value="<?php echo $ligne->categorie;?>" > <?php echo $ligne->categorie;?></option>
+					?>
+						<option value="<?php echo $ligne->categorie;?>"><?php echo $ligne->categorie;?></option>
 						<?php
 						}
 						?>
-						</select>
-						</td>
-						</tr>
-	  
-				<tr><td><div align="right" colspan=3><input type="submit" name="Valider"></div></td></tr>
-				
-			
-			</table>
-			</form>
-			</div>
+					</select>
+				</td>
+			</tr>
+	  		<tr><td><div align="right" colspan=3><input type="submit" name="Valider"></div></td></tr>
+		</table>
+	</form>
+	</div>
 <?php
 
 }
