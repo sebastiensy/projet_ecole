e62912eb6089244534415ec710873a4cb4472e83
florianD <floriand_@hotmail.fr>
2015-05-13 18:01:03 +0200

    modifs

--- web/admin/gestion_article/modif_article.php
+++ web/admin/gestion_article/modif_article.php
@@ -24,36 +24,19 @@ require_once('../inc/data.inc.php');
 
 		<div align="center" id="add-form">
 		<form method="post" action="modif_article.php?p=recherche" id="maj">
-		
-		<table width="90%" align="center">
-			<tr>
-			<td width="40%">
-			Reference
-			</td>
-			
-			<td width="50%">
-			Description
-					
-					</td>
+			<table width="90%" align="center">
+				<tr>
+					<td width="40%">Référence</td>
+					<td width="50%">Description</td>
 					<td></td>  
-			</tr>
-					
-			<tr>
-				<td width="40%">
-					<div align="Left">
-						<span><input type="text" name="ref"></span>
-					</div>
-				</td>
-				<td width="50%">
-					
-								<span><input type="text" name="desc"></span>
-					
-				</td>
-				<span><td><div align="right"><a href="#" onclick="subm();" class="myButton" id="sub">Rechercher</a></div></td><input style="font-size:0px; width:0px; height:0px;border:0px;padding:0px;" type="submit" value="" name="sub"/></span>
-				
-			</tr>
+				</tr>
+				<tr>
+					<td width="40%"><div align="Left"><span><input type="text" name="ref"></span></div></td>
+					<td width="50%"><span><input type="text" name="desc"></span></td>
+					<span><td><div align="right"><a href="#" onclick="subm();" class="myButton" id="sub">Rechercher</a></div></td><input style="font-size:0px; width:0px; height:0px;border:0px;padding:0px;" type="submit" value="" name="sub"/></span>
+				</tr>
 			</table>
-			</form>
+		</form>
 
 <?php
 
@@ -79,65 +62,38 @@ if(isset($_GET['p']))
 		else
 		{
 			$req.="1";
-			}
-	
-		
+		}
 		$db = new DB_connection();
 		$db->DB_query($req);
-		
-		
 	}
 	
-	}
-?>
-<!--<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="../../../css/style1.css" />
-		<script type="text/javascript">
-		function subm()
+}
+
+if(isset($_GET['p']))
+{
+	if($_GET['p']=="recherche")
+	{
+		echo '<table width="90%" align="center">';
+		while($ligne=$db->DB_object())
 		{
-			document.getElementById('maj').submit();
+			echo '<tr><td width="10%">';
+			echo $ligne->ref_mat;
+			echo '</td><td width="60%"><form method="post" action="modif_article1.php?p=modif_desc&ref='.$ligne->ref_mat.'" >
+			<div><input size=50 type=text name="desc" value="'.$ligne->desc_mat.'" >			
+			<INPUT border=0 src="../../../img/icon_OK.png" type=image Value=submit ></div></form></td>';
+			
+			echo '<td width="30%"><form method="post" action="modif_article1.php?p=modif_prix&ref='.$ligne->ref_mat.'&prix='.$ligne->prix_mat.'&id='.$ligne->id_mat.'" >
+			<div><input size=10 type=text name="pr" value="'.$ligne->prix_mat.'">			
+			<INPUT border=0 src="../../../img/icon_OK.png" type=image Value=submit></div></form></td>';
+			echo '<td width="5%"><form method="post" action="modif_article1.php?p=delete&ref='.$ligne->ref_mat.'&prix='.$ligne->prix_mat.'&id='.$ligne->id_mat.'" >
+			<INPUT border=0 src="../../../img/del.png" type=image Value=submit></div></form></td>';
+			echo '</tr>';
 		}
-		
-		</script>
-		
-	</head>
-	
-	<body>
-	<style type="text/css">
-		body {
-			
-			/*background-color: #FEF3DB;*/
-			background-image:none;
-			}
-  </style>-->
-		
-			
-		<?php
-		if(isset($_GET['p']))
-		{
-			if($_GET['p']=="recherche")
-			{
-				echo '<table width="90%" align="center">';
-				while($ligne=$db->DB_object())
-				{
-					echo '<tr><td width="10%">';
-					echo $ligne->ref_mat;
-					echo '</td><td width="60%"><form method="post" action="modif_article1.php?p=modif_desc&ref='.$ligne->ref_mat.'" >
-					<div><input size=50 type=text name="desc" value="'.$ligne->desc_mat.'" >			
-					<INPUT border=0 src="../../../img/icon_OK.png" type=image Value=submit ></div></form></td>';
-					
-					echo '<td width="30%"><form method="post" action="modif_article1.php?p=modif_prix&ref='.$ligne->ref_mat.'&prix='.$ligne->prix_mat.'&id='.$ligne->id_mat.'" >
-					<div><input size=10 type=text name="pr" value="'.$ligne->prix_mat.'">			
-					<INPUT border=0 src="../../../img/icon_OK.png" type=image Value=submit></div></form></td>';
-					echo '<td width="5%"><form method="post" action="modif_article1.php?p=delete&ref='.$ligne->ref_mat.'&prix='.$ligne->prix_mat.'&id='.$ligne->id_mat.'" >
-					<INPUT border=0 src="../../../img/del.png" type=image Value=submit></div></form></td>';
-					echo '</tr>';
-					}
-					echo '</table>';
-				}
-				}
-			?>
+		echo '</table>';
+	}
+}
+
+?>
 
 <?php
 
