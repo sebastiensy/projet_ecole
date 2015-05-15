985c420c976760b859c5c754f9d1433d8232fc3d
sebastiensy <seb6890@gmail.com>
2015-05-15 11:33:32 +0200

    par défaut on laisse fermer les accordéons

--- js/chargement_page_suivi.js
+++ js/chargement_page_suivi.js
@@ -1,11 +1,15 @@
 $( document ).ready(function() {
 		var url = window.location.href;
 		var nb0 = /nb\=[0-9]+$/.test(url);
-    	$( "#accordion-resizer" ).accordion();
+    	$("#accordion-resizer").accordion({ header: "h3", active: false, collapsible: true });
 		if (nb0)
 		{
 			var nb1 = /nb\=[0-9]+$/.exec(url);
 			var nb2 = parseInt(/[0-9]+/.exec(nb1));
 			$( "#accordion-resizer" ).accordion( "option", "active", nb2 );
+		}
+		else
+		{
+	    	$("#accordion-resizer").accordion({ header: "h3", active: false, collapsible: true });
 		}
 });
