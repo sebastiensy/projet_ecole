61d8b8ef3fcd3914ede47322b4661a052e48e127
sebastiensy <seb6890@gmail.com>
2015-05-20 16:58:20 +0200

    mise en page panier messagerie admin

---
+++ web/messagerie/index.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once('../../inc/data.inc.php');
+require_once(LIB.'/lib_form_contacter.php');
+require_once(LIB.'/lib_verifications.php');
+
+?>
+
+<body id="back">
+
+	<div id="banner">
+	</div>
+
+	<div class="menu">
+
+		<div id="connexion">
+			<?php
+				require_once("../connexion/login.php");
+			?>
+		</div> 
+
+		<div id="menu">
+
+			<div id="menu1">
+				<a href="../"><img src="../../img/menu/accueil.png"></a>
+				<a href="../fournitures/"><img src="../../img/menu/article.png"></a>
+				<?php 
+				if(!isset($_SESSION['id_parent']))
+				{
+					?>
+					<a href="../inscription/"><img src="../../img/menu/inscription.png"></a>
+					<?php
+				}
+				else
+				{
+					?>
+					<a href="../compte/"><img src="../../img/menu/compte.png"></a>
+					<?php 
+				}
+				?>
+				<a href="../contact/"><img src="../../img/menu/contact.png"></a>
+			</div>
+
+		</div>
+
+		<div id="messagerie">
+			<a href="../messagerie/"><img src="../../img/menu/messagerie.png"></a>
+		</div>
+
+		<div id="panier">
+			<a href="../panier/"><img src="../../img/menu/panier.png"></a>
+		</div>
+
+		<?php 
+				if(isset($_SESSION['droits']))
+				{
+					if ($_SESSION['droits'] ==1 )
+					{
+					?>
+					<div id="admin">
+						<a href="../admin/"><img src="../../img/menu/admin.png"></a>
+					</div>
+					<?php
+					}
+				}
+
+		?>
+
+	</div>
+
+	<div class="corps">
+	<div id="page">
+
+	<p class="titre">Messagerie</p>
+	<?php
+
+		
+
+	?>
+
+	</div>
+	</div>
+
+<?php
+
+require_once(INC.'/footer.inc.php');
+	
+?>
