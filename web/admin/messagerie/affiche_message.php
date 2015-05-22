27ed7d75a00f257a47aa55bd0b7ff2a32de00d80
florianD <floriand_@hotmail.fr>
2015-05-22 17:21:28 +0200

    modifs

--- web/admin/messagerie/affiche_message.php
+++ web/admin/messagerie/affiche_message.php
@@ -15,51 +15,40 @@ if (isset($_GET['id']))
 	$id_msg = $_GET['id'];
 
 	$requete2 = 'UPDATE Message set lu = 1 WHERE id_message = '.$id_msg;
-		
 
 	$requete = 'SELECT email_parent, objet, message, jma FROM Message WHERE id_message = '.$id_msg;
 
 	$db = new DB_connection();
 	$db->DB_query($requete);
 
-		
-		while($msg = $db->DB_object())
-		{
-			?>
-
-			<table width="800" align="left" class="data">
-			<tr>
-				<th width="90" ><div align="center">N° message</div></th>
-				<td><?php echo $id_msg;?></td>
-			</tr>
-			<tr>
-				<th width="90" ><div align="center">Expediteur</div></th>
-				<?php echo "<td>".$msg->email_parent."</td>";?>
-			</tr>
-			<tr>
-				<th width="90" ><div align="center">Objet</div></th>
-				<?php echo "<td>".$msg->objet."</td>";?>
-			</tr>
-			<tr>
-				<th width="90" ><div align="center">Date</div></th>
-				<?php echo "<td>".date("d-m-Y", strtotime($msg->jma))."</td>";?>
-			</tr>
-			<tr>
-				<th width="90" ><div align="center">Message</div></th>
-				<?php echo "<td>".$msg->message."</td>";?>
-			</tr>
-			</table>
-
-			<?php
-			
-
-
-		}
-
-		$db->DB_query($requete2);
-
-		
-		$db->DB_done();
-		
+	while($msg = $db->DB_object())
+	{
+		?>
+		<table width="800" align="left" class="data">
+		<tr>
+			<th width="90" ><div align="center">N° message</div></th>
+			<td><?php echo $id_msg;?></td>
+		</tr>
+		<tr>
+			<th width="90" ><div align="center">Expediteur</div></th>
+			<?php echo "<td>".$msg->email_parent."</td>";?>
+		</tr>
+		<tr>
+			<th width="90" ><div align="center">Objet</div></th>
+			<?php echo "<td>".$msg->objet."</td>";?>
+		</tr>
+		<tr>
+			<th width="90" ><div align="center">Date</div></th>
+			<?php echo "<td>".date("d-m-Y", strtotime($msg->jma))."</td>";?>
+		</tr>
+		<tr>
+			<th width="90" ><div align="center">Message</div></th>
+			<?php echo "<td>".$msg->message."</td>";?>
+		</tr>
+		</table>
+		<?php
+	}
+	$db->DB_query($requete2);
+	$db->DB_done();
 }
 ?>
--- web/notification/affiche_message.php
+++ web/notification/affiche_message.php
@@ -12,48 +12,44 @@ require_once(LIB.'/lib_db.class.php');
 	</head>
 	<body>
 
-
 <?php
 
-if (isset($_GET['id']))
+if(isset($_GET['id']))
 {
 	$id_msg = $_GET['id'];
+
+	$requete2 = 'UPDATE Message set lu = 1 WHERE id_message = '.$id_msg;
 
 	$requete = 'SELECT objet, message, jma FROM Message WHERE id_message = '.$id_msg;
 
 	$db = new DB_connection();
 	$db->DB_query($requete);
 
-		
-		while($msg = $db->DB_object())
-		{
-			?>
-
-			<table width="500" align="left" class="data">
-			<!-- <tr>
-				<th width="90" ><div align="center">N° message</div></th>
-				<td><?php /*echo $id_msg;*/ ?></td>
-			</tr> -->
-			<tr>
-				<th width="90" ><div align="right">Objet : </div></th>
-				<?php echo "<td>".$msg->objet."</td>";?>
-			</tr>
-			<tr>
-				<th width="90" ><div align="right">Date : </div></th>
-				<?php echo "<td>".date("d-m-Y", strtotime($msg->jma))."</td>";?>
-			</tr>
-			<tr>
-				<th width="90" ><div align="right">Message : </div></th>
-				<?php echo "<td>".$msg->message."</td>";?>
-			</tr>
-			</table>
-
-			<?php
-			
-
-
-		}
-
-		
+	while($msg = $db->DB_object())
+	{
+		?>
+		<table width="500" align="left" class="data">
+		<!-- <tr>
+			<th width="90" ><div align="center">N° message</div></th>
+			<td><?php /*echo $id_msg;*/ ?></td>
+		</tr> -->
+		<tr>
+			<th width="90" ><div align="right">Objet : </div></th>
+			<?php echo "<td>".$msg->objet."</td>";?>
+		</tr>
+		<tr>
+			<th width="90" ><div align="right">Date : </div></th>
+			<?php echo "<td>".date("d-m-Y", strtotime($msg->jma))."</td>";?>
+		</tr>
+		<tr>
+			<th width="90" ><div align="right">Message : </div></th>
+			<?php echo "<td>".$msg->message."</td>";?>
+		</tr>
+		</table>
+		<?php
+	}
+	$db->DB_query($requete2);
+	$db->DB_done();
 }
+
 ?>
