1398e1587613eba6747d00f2d33659496725874b
florianD <floriand_@hotmail.fr>
2015-05-24 17:50:21 +0200

    correction problème header (date limite)

--- web/admin/gestion_site/gestion_site.php
+++ web/admin/gestion_site/gestion_site.php
@@ -11,6 +11,29 @@ require_once('../inc/droits.inc.php');
 
 <div class="corps">
 
+	<?php
+
+	if(isset($_POST['date_cache']))
+	{
+		$db = new DB_connection();
+		$requete = 'SELECT jma FROM Date_limite';
+		$db->DB_query($requete);
+		if ($db->DB_count() == 0)
+		{
+			$inserer = 'INSERT INTO Date_limite (jma) VALUES ("'.$_POST['date_cache'].'")';
+			$db->DB_query($inserer);
+		}
+		else
+		{
+			$modifier = 'UPDATE Date_limite SET jma = "'.$_POST['date_cache'].'"';
+			$db->DB_query($modifier);
+		}
+
+		header('Location: index.php');
+	}
+
+	?>
+
 	<div id="menu">
 		<?php
 		require_once('../inc/menu.inc.php');
@@ -22,48 +45,25 @@ require_once('../inc/droits.inc.php');
 <?php
 
 if(isset($_GET['gestion']) && isset($_GET['date']))
-	{
-		$tmp = explode('/', $_GET['date']);
-		$d = $tmp[2].'-'.$tmp[1].'-'.$tmp[0];
-		?>
-		<table width="900" align="center" class="entete">
-			<tr>
-				<td><div align="right">Gestion du site</div></td>
-			</tr>
-		</table>
-		<br><br>
-		<p>Veuillez saisir la date au format jj/mm/aaaa</p>
-		<form method="post" action="gestion_site.php">
-		<p><label class="gestion_site" for="jma">Date limite des commandes : </label><input type="text" id="date_limite" name="date_limite" value="<?php echo $_GET['date']; ?>" /></p>
-		<input type="text" id="date_cache" name="date_cache" value="<?php echo $d; ?>" hidden/>
-		<input type="submit" value="Enregistrer">
-		</form>
-	<?php
-	}
-	?>
-
-<?php
-
-if (isset($_POST['date_cache']))
 {
-	$db = new DB_connection();
-	$requete = 'SELECT jma FROM Date_limite';
-	$db->DB_query($requete);
-	if ($db->DB_count() == 0)
-	{
-		$inserer = 'INSERT INTO Date_limite (jma) VALUES ("'.$_POST['date_cache'].'")';
-		$db->DB_query($inserer);
-	}
-	else
-	{
-		$modifier = 'UPDATE Date_limite SET jma = "'.$_POST['date_cache'].'"';
-		$db->DB_query($modifier);
-	}
-
-	header('Location: index.php');
+	$tmp = explode('/', $_GET['date']);
+	$d = $tmp[2].'-'.$tmp[1].'-'.$tmp[0];
+	?>
+	<table width="900" align="center" class="entete">
+		<tr>
+			<td><div align="right">Gestion du site</div></td>
+		</tr>
+	</table>
+	<br><br>
+	<p>Veuillez saisir la date au format jj/mm/aaaa</p>
+	<form method="post" action="gestion_site.php">
+	<p><label class="gestion_site" for="jma">Date limite des commandes : </label><input type="text" id="date_limite" name="date_limite" value="<?php echo $_GET['date']; ?>" /></p>
+	<input type="text" id="date_cache" name="date_cache" value="<?php echo $d; ?>" hidden/>
+	<input type="submit" value="Enregistrer">
+	</form>
+<?php
 }
-
-?>	
+?>
 
 <?php
 
