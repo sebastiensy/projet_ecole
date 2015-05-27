a3bb3c4ed613ee18e569d4f5cc600f5b6a7f42da
sebastiensy <seb6890@gmail.com>
2015-05-27 11:59:23 +0200

    imprimer en pdf en cours

--- web/admin/commande_fournisseur/test.php
+++ web/admin/commande_fournisseur/test.php
@@ -1,10 +1,132 @@
 <?php
 require('../../../lib/fpdf17/fpdf.php');
 
-$pdf=new FPDF();
-$pdf->Open();
+class PDF extends FPDF
+{
+	// En-tête
+	function Header()
+	{
+	    // Logo
+	    $this->Image('../../../img/logo.png',10,6,70);
+	    // Police Arial gras 15
+	    $this->SetFont('Arial','B',15);
+	    // Décalage à droite
+	    $this->Cell(100);
+	    // Titre
+	    $this->Cell(80,10,'Commande fournisseur',1,0,'C');
+	    // Saut de ligne
+	    $this->Ln(20);
+	}
+
+	// Pied de page
+	function Footer()
+	{
+	    // Positionnement à 1,5 cm du bas
+	    $this->SetY(-15);
+	    // Police Arial italique 8
+	    $this->SetFont('Arial','I',8);
+	    // Numéro de page
+	    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+	}
+
+	// Chargement des données
+	function LoadData($file)
+	{
+	    // Lecture des lignes du fichier
+	    $lines = file($file);
+	    $data = array();
+	    foreach($lines as $line)
+	        $data[] = explode(';',trim($line));
+	    return $data;
+	}
+
+	// Tableau simple
+	function BasicTable($header, $data)
+	{
+	    // En-tête
+	    foreach($header as $col)
+	        $this->Cell(50,7,$col,1);
+	    $this->Ln();
+	    // Données
+	    foreach($data as $row)
+	    {
+	        foreach($row as $col)
+            	$this->Cell(50,6,$col,1,0,"C");
+  	        $this->Ln();
+	    }
+	}
+
+	// Tableau amélioré
+	function ImprovedTable($header, $data)
+	{
+	    // Largeurs des colonnes
+	    $w = array(30,100, 30, 30);
+	    // En-tête
+	    for($i=0;$i<count($header);$i++)
+	        $this->Cell($w[$i],7,$header[$i],1,0,'C');
+	    $this->Ln();
+	    // Données
+	    foreach($data as $row)
+	    {
+	        $this->Cell($w[0],6,$row[0],'LR',0,'C');
+	        $this->Cell($w[1],6,$row[1],'LR',0,'C');
+	        $this->Cell($w[2],6,$row[2],'LR',0,'C');
+	        $this->Cell($w[3],6,$row[3],'LR',0,'C');
+	        //$this->Cell($w[2],6,number_format($row[2],0,',',' '),'LR',0,'R');
+	        //$this->Cell($w[3],6,number_format($row[3],0,',',' '),'LR',0,'R');
+	        $this->Ln();
+	    }
+	    // Trait de terminaison
+	    $this->Cell(array_sum($w),0,'','T');
+	}
+
+	// Tableau coloré
+	function FancyTable($header, $data)
+	{
+	    // Couleurs, épaisseur du trait et police grasse
+	    $this->SetFillColor(255,0,0);
+	    $this->SetTextColor(255);
+	    $this->SetDrawColor(128,0,0);
+	    $this->SetLineWidth(.3);
+	    $this->SetFont('','B');
+	    // En-tête
+	    $w = array(40, 35, 45, 40);
+	    for($i=0;$i<count($header);$i++)
+	        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+	    $this->Ln();
+	    // Restauration des couleurs et de la police
+	    $this->SetFillColor(224,235,255);
+	    $this->SetTextColor(0);
+	    $this->SetFont('');
+	    // Données
+	    $fill = false;
+	    foreach($data as $row)
+	    {
+	        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+	        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+	        $this->Cell($w[2],6,number_format($row[2],0,',',' '),'LR',0,'R',$fill);
+	        $this->Cell($w[3],6,number_format($row[3],0,',',' '),'LR',0,'R',$fill);
+	        $this->Ln();
+	        $fill = !$fill;
+	    }
+	    // Trait de terminaison
+	    $this->Cell(array_sum($w),0,'','T');
+	}
+}
+
+$pdf = new PDF();
+$pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World !');
+$pdf->SetFont('Arial','',12);
+
+
+// Titres des colonnes
+$header = array('Reference', 'Materiel', 'Quantite', 'Prix');
+
+// Chargement des données
+$data = $pdf->LoadData('cmdF.txt');
+$pdf->SetFont('Arial','',12);
+$pdf->ImprovedTable($header,$data);
+
 $pdf->Output();
 ?>
