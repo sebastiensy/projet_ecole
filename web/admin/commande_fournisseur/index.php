b8fa7c0ef6d99519663adcb8a9bdac6a0e1da034
sebastiensy <seb6890@gmail.com>
2015-05-28 11:24:28 +0200

    pdf cmd fournisseur ok

--- web/admin/commande_fournisseur/index.php
+++ web/admin/commande_fournisseur/index.php
@@ -29,125 +29,10 @@ require_once('../inc/droits.inc.php');
 
 <?php
 
-/*
- * requete pour liste
- */
-$requete1 = 'SELECT SUM(cp.qte_scat*i.exemplaire) as qte , mat.desc_mat, mat.prix_mat, mat.id_mat
-	FROM Compose as cp, Materiel as mat, Inclus as i, Commande as com
-    WHERE mat.id_mat = cp.id_mat AND cp.id_nivliste = i.id_nivliste AND i.id_commande = com.id_commande
-    AND com.etat = 2
-    GROUP BY mat.id_mat';
-
-/*
- * requete pour les fournitures seules
- */
-$requete2 = 'SELECT SUM(c.quantite) as qte, m.desc_mat, m.prix_mat, m.id_mat 
-	FROM Contient as c, Materiel as m, Commande as com 
-	WHERE c.id_mat = m.id_mat AND com.id_commande = c.id_commande 
-	AND com.etat = 2
-    GROUP BY c.id_mat';
-
-$tab = array();
-
-$db = new DB_connection();
-$db->DB_query($requete1);
-
-$prix = array();
-
-while($elem = $db->DB_object())
-{
-	if(isset($tab[$elem->id_mat]))
-		$tab[$elem->id_mat] += $elem->qte;
-	else
-		$tab[$elem->id_mat] = $elem->qte;	
-}
-
-$db->DB_query($requete2);
-
-while($elem = $db->DB_object())
-{
-	if(isset($tab[$elem->id_mat]))
-		$tab[$elem->id_mat] += $elem->qte;
-	else
-		$tab[$elem->id_mat] = $elem->qte;	
-}
-
-$ids = array_keys($tab);
-
-
-/*
-*	ouverture du fichier
-*/
-$fichierCmdF = fopen('cmdF.txt', 'w+');
-
-
-
-if(!empty($ids))
-{
-	$requete3 = 'SELECT id_mat, ref_mat, desc_mat, prix_mat FROM Materiel WHERE id_mat IN ('.implode(',',$ids).')';
-	$db->DB_query($requete3);
-
-	if($db->DB_count() > 0)
-	{
-		?>
-		<table width="800" align="center" class="data">
-			<tr>
-				<th width="90" ><div align="center">Reference</div></th>
-				<th width="90" ><div align="center">Materiel</div></th>
-				<th width="90" ><div align="center">Quantite</div></th>
-				<th width="90" ><div align="center">Prix</div></th>
-			</tr>
-		<?php
-		while($liste = $db->DB_object())
-		{
-			echo "<tr><td><div align='center'>".$liste->ref_mat."</div></td>";
-			echo "<td><div align='center'>".$liste->desc_mat."</div></td>";
-			echo "<td><div align='center'>".$tab[$liste->id_mat]."</div></td>";
-			echo "<td><div align='center'>".number_format($liste->prix_mat, 2, ',', ' ')." €</div></td>";
-			array_push($prix, $tab[$liste->id_mat] * $liste->prix_mat);
-
-			/*
-			*	ecriture dans le fichier
-			*/
-			fputs($fichierCmdF, $liste->ref_mat.';');
-			fputs($fichierCmdF, $liste->desc_mat.';');
-			fputs($fichierCmdF, $tab[$liste->id_mat].';');
-			fputs($fichierCmdF, number_format($liste->prix_mat, 2, ',', ' ').';');
-			fputs($fichierCmdF, "\r\n");
-		}
-	
-		echo "</tr>";
-		echo "</table>";
-		echo "<br>";
-
-		$somme = array_sum($prix);
-		echo "<div align='center'><strong style='color: red'>TOTAL : ".number_format($somme, 2, ',', ' '). " €</strong></div>";
-
-
-		/*
-		*	fermeture du fichier
-		*/
-		fclose($fichierCmdF);
-
-		/*
-		*	imprimer en pdf
-		*/
-		echo "<form method='POST' action='index.php'><a href='pdf.php?pt=".$somme."'><img src='../../../img/imprimer.png' id='impCmdF' border='0'></a>";
-
-		/*
-		*	passer la commande au fournisseur
-		*/
-		echo "<input type='submit' id='btnCmdF' name='cmdFournisseur' value='Passer commande fournisseur'></input></form>";
-
-	}
-}
-else
-{
-	echo "<p>Il n'y a pas de commandes validées.</p>";
-}
-
 if (isset($_POST['cmdFournisseur']))
 {
+	$db = new DB_connection();
+
 	/*
 	*	envoi une notification aux parents qui passent de validé à commande fournisseur
 	*/
@@ -178,6 +63,129 @@ if (isset($_POST['cmdFournisseur']))
 	echo "<p>Toutes les commandes validées sont passées à l'état \"Commande fournisseur\".</p>";
 
 }
+else
+{
+	/*
+	 * requete pour liste
+	 */
+	$requete1 = 'SELECT SUM(cp.qte_scat*i.exemplaire) as qte , mat.desc_mat, mat.prix_mat, mat.id_mat
+		FROM Compose as cp, Materiel as mat, Inclus as i, Commande as com
+	    WHERE mat.id_mat = cp.id_mat AND cp.id_nivliste = i.id_nivliste AND i.id_commande = com.id_commande
+	    AND com.etat = 2
+	    GROUP BY mat.id_mat';
+
+	/*
+	 * requete pour les fournitures seules
+	 */
+	$requete2 = 'SELECT SUM(c.quantite) as qte, m.desc_mat, m.prix_mat, m.id_mat 
+		FROM Contient as c, Materiel as m, Commande as com 
+		WHERE c.id_mat = m.id_mat AND com.id_commande = c.id_commande 
+		AND com.etat = 2
+	    GROUP BY c.id_mat';
+
+	$tab = array();
+	
+	$db = new DB_connection();
+	$db->DB_query($requete1);
+
+	$prix = array();
+
+	while($elem = $db->DB_object())
+	{
+		if(isset($tab[$elem->id_mat]))
+			$tab[$elem->id_mat] += $elem->qte;
+		else
+			$tab[$elem->id_mat] = $elem->qte;	
+	}
+
+	$db->DB_query($requete2);
+
+	while($elem = $db->DB_object())
+	{
+		if(isset($tab[$elem->id_mat]))
+			$tab[$elem->id_mat] += $elem->qte;
+		else
+			$tab[$elem->id_mat] = $elem->qte;	
+	}
+
+	$ids = array_keys($tab);
+
+
+	/*
+	*	ouverture du fichier
+	*/
+	//$fichierCmdF = fopen('cmdF.txt', 'w+');
+
+
+
+	if(!empty($ids))
+	{
+		$requete3 = 'SELECT id_mat, ref_mat, desc_mat, prix_mat FROM Materiel WHERE id_mat IN ('.implode(',',$ids).')';
+		$db->DB_query($requete3);
+
+		if($db->DB_count() > 0)
+		{
+			?>
+			<table width="800" align="center" class="data">
+				<tr>
+					<th width="90" ><div align="center">Reference</div></th>
+					<th width="90" ><div align="center">Materiel</div></th>
+					<th width="90" ><div align="center">Quantite</div></th>
+					<th width="90" ><div align="center">Prix</div></th>
+				</tr>
+			<?php
+			while($liste = $db->DB_object())
+			{
+				echo "<tr><td><div align='center'>".$liste->ref_mat."</div></td>";
+				echo "<td><div align='center'>".$liste->desc_mat."</div></td>";
+				echo "<td><div align='center'>".$tab[$liste->id_mat]."</div></td>";
+				echo "<td><div align='center'>".number_format($liste->prix_mat, 2, ',', ' ')." €</div></td>";
+				array_push($prix, $tab[$liste->id_mat] * $liste->prix_mat);
+
+				/*
+				*	ecriture dans le fichier
+				*/
+				/*fputs($fichierCmdF, $liste->ref_mat.';');
+				fputs($fichierCmdF, $liste->desc_mat.';');
+				fputs($fichierCmdF, $tab[$liste->id_mat].';');
+				fputs($fichierCmdF, number_format($liste->prix_mat, 2, ',', ' ').';');
+				fputs($fichierCmdF, "\r\n");*/
+			}
+		
+			echo "</tr>";
+			echo "</table>";
+			echo "<br>";
+
+			$somme = array_sum($prix);
+			echo "<div align='center'><strong style='color: red'>TOTAL : ".number_format($somme, 2, ',', ' '). " €</strong></div>";
+
+
+			/*
+			*	fermeture du fichier
+			*/
+			//fclose($fichierCmdF);
+
+			/*
+			*	imprimer en pdf
+			*/
+			echo "<form method='POST' action='index.php'><a href='pdf.php?pt=".$somme."'><img src='../../../img/imprimer.png' id='impCmdF' border='0'></a>";
+
+			/*
+			*	passer la commande au fournisseur
+			*/
+			echo "<input type='submit' id='btnCmdF' name='cmdFournisseur' value='Passer commande fournisseur'></input></form>";
+
+		}
+	}
+	else
+	{
+		echo "<p>Il n'y a pas de commandes validées.</p>";
+	}
+
+}
+
+
+
 
 ?>
 
--- web/admin/commande_fournisseur/pdf.php
+++ web/admin/commande_fournisseur/pdf.php
@@ -1,6 +1,11 @@
 <?php
-require('../../../lib/fpdf17/fpdf.php');
 
+require('../../../lib/fpdf17/fpdf.php');
+require_once('../inc/data.inc.php');
+
+?>
+
+<?php
 class PDF extends FPDF
 {
 	// En-tête
@@ -65,6 +70,89 @@ class PDF extends FPDF
 
 }
 
+function fichier() 
+{
+	/*
+	 * requete pour liste
+	 */
+	$requete1 = 'SELECT SUM(cp.qte_scat*i.exemplaire) as qte , mat.desc_mat, mat.prix_mat, mat.id_mat
+		FROM Compose as cp, Materiel as mat, Inclus as i, Commande as com
+	    WHERE mat.id_mat = cp.id_mat AND cp.id_nivliste = i.id_nivliste AND i.id_commande = com.id_commande
+	    AND com.etat = 2
+	    GROUP BY mat.id_mat';
+
+	/*
+	 * requete pour les fournitures seules
+	 */
+	$requete2 = 'SELECT SUM(c.quantite) as qte, m.desc_mat, m.prix_mat, m.id_mat 
+		FROM Contient as c, Materiel as m, Commande as com 
+		WHERE c.id_mat = m.id_mat AND com.id_commande = c.id_commande 
+		AND com.etat = 2
+	    GROUP BY c.id_mat';
+
+	$tab = array();
+	
+	$db = new DB_connection();
+	$db->DB_query($requete1);
+
+	$prix = array();
+
+	while($elem = $db->DB_object())
+	{
+		if(isset($tab[$elem->id_mat]))
+			$tab[$elem->id_mat] += $elem->qte;
+		else
+			$tab[$elem->id_mat] = $elem->qte;	
+	}
+
+	$db->DB_query($requete2);
+
+	while($elem = $db->DB_object())
+	{
+		if(isset($tab[$elem->id_mat]))
+			$tab[$elem->id_mat] += $elem->qte;
+		else
+			$tab[$elem->id_mat] = $elem->qte;	
+	}
+
+	$ids = array_keys($tab);
+
+
+	/*
+	*	ouverture du fichier
+	*/
+	$fichierCmdF = fopen('cmdF.txt', 'w+');
+
+
+	if(!empty($ids))
+	{
+		$requete3 = 'SELECT id_mat, ref_mat, desc_mat, prix_mat FROM Materiel WHERE id_mat IN ('.implode(',',$ids).')';
+		$db->DB_query($requete3);
+
+		if($db->DB_count() > 0)
+		{
+			while($liste = $db->DB_object())
+			{
+				/*
+				*	ecriture dans le fichier
+				*/
+				fputs($fichierCmdF, $liste->ref_mat.';');
+				fputs($fichierCmdF, $liste->desc_mat.';');
+				fputs($fichierCmdF, $tab[$liste->id_mat].';');
+				fputs($fichierCmdF, number_format($liste->prix_mat, 2, ',', ' ').';');
+				fputs($fichierCmdF, "\r\n");
+			}
+		
+			$somme = array_sum($prix);
+
+			/*
+			*	fermeture du fichier
+			*/
+			fclose($fichierCmdF);
+		}
+	}
+}
+
 
 if (isset($_GET['pt']))
 {
@@ -78,6 +166,9 @@ if (isset($_GET['pt']))
 
 	// Titres des colonnes
 	$header = array('Reference', 'Materiel', 'Quantite', 'Prix');
+
+	// Ecrit dans le fichier
+	fichier();
 
 	// Chargement des données du fichier
 	$data = $pdf->LoadData('cmdF.txt');
